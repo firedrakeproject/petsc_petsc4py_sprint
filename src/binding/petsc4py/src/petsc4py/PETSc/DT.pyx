775bafbe3304faae8ab5b99ae6fe86bb2f971517
Marcin Rogowski <marcin.rogowski@kaust.edu.sa>
2023-03-28 16:46:04 +0100

    WIP

--- src/binding/petsc4py/src/petsc4py/PETSc/DT.pyx
+++ src/binding/petsc4py/src/petsc4py/PETSc/DT.pyx
@@ -1,56 +1,167 @@
 # --------------------------------------------------------------------
 
 cdef class Quad(Object):
-
+    """
+    Quadrature rule for integration.
+    """
     def __cinit__(self):
         self.obj = <PetscObject*> &self.quad
         self.quad = NULL
 
-    def view(self, Viewer viewer=None):
+    def view(self, Viewer viewer=None) -> None:
+        """View a `Quad` object
+
+        Collective.
+
+        Parameters
+        ----------
+        viewer
+            A `Viewer` to display the graph.
+
+        See also
+        --------
+        petsc.PetscQuadratureView
+
+        """
         cdef PetscViewer vwr = NULL
         if viewer is not None: vwr = viewer.vwr
         CHKERR( PetscQuadratureView(self.quad, vwr) )
 
-    def create(self, comm=None):
+    def create(self, comm: Comm | None = None) -> Self:
+        """Create a `Quad` object
+
+        Collective.
+
+        Parameters
+        ----------
+        comm
+            The communicator for the `Quad` object.
+
+        See also
+        --------
+        petsc.PetscQuadratureCreate
+
+        """
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
         cdef PetscQuadrature newquad = NULL
         CHKERR( PetscQuadratureCreate(ccomm, &newquad) )
         PetscCLEAR(self.obj); self.quad = newquad
         return self
 
-    def duplicate(self):
+    def duplicate(self) -> Quad:
+        """Create a deep copy of the `Quad` object.
+
+        Collective.
+
+        See also
+        --------
+        petsc.PetscQuadratureDuplicate
+
+        """
         cdef Quad newquad = Quad()
         CHKERR( PetscQuadratureDuplicate(self.quad, &newquad.quad) )
         return newquad
 
-    def destroy(self):
+    def destroy(self) -> Self:
+        """Destroys the `Quad` object
+
+        Collective.
+
+        See also
+        --------
+        petsc.PetscQuadratureDestroy
+
+        """
         CHKERR( PetscQuadratureDestroy(&self.quad) )
         return self
 
-    def getData(self):
-        cdef PetscInt cdim = 0 
+    def getData(self) -> tuple(ndarray, ndarray):
+        """Return the data defining the `Quad`.
+
+        Not collective.
+
+        Returns
+        -------
+        points : ndarray
+            The quadrature points.
+        weights : ndarray
+            The quadrature weights.
+
+        See also
+        --------
+        petsc.PetscQuadratureGetData
+
+        """
+        cdef PetscInt cdim = 0
         cdef PetscInt cnc = 0
         cdef PetscInt cnpoints = 0
         cdef const PetscReal *cpoints = NULL
         cdef const PetscReal *cweights = NULL
         CHKERR( PetscQuadratureGetData(self.quad, &cdim, &cnc, &cnpoints, &cpoints, &cweights))
         return array_r(cnpoints*cdim, cpoints), array_r(cnpoints*cnc, cweights)
-    
-    def getNumComponents(self):
+
+    def getNumComponents(self) -> int:
+        """Return the number of components for functions to be integrated
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscQuadratureGetNumComponents
+
+        """
         cdef PetscInt cnc = 0
         CHKERR( PetscQuadratureGetNumComponents(self.quad, &cnc) )
         return toInt(cnc)
 
-    def setNumComponents(self, nc):
+    def setNumComponents(self, nc: int) -> None:
+        """Return the number of components for functions to be integrated.
+
+        Not collective.
+
+        Parameters
+        ----------
+        nc
+            The number of components.
+
+        See also
+        --------
+        petsc.PetscQuadratureSetNumComponents
+
+        """
         cdef PetscInt cnc = asInt(nc)
         CHKERR( PetscQuadratureSetNumComponents(self.quad, cnc) )
 
-    def getOrder(self):
+    def getOrder(self) -> int:
+        """Return the order of the method in the `Quad`.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscQuadratureGetOrder
+
+        """
         cdef PetscInt corder = 0
         CHKERR( PetscQuadratureGetOrder(self.quad, &corder))
         return toInt(corder)
 
-    def setOrder(self, order):
+    def setOrder(self, order: int) -> None:
+        """Set the order of the method in the `Quad`.
+
+        Not collective.
+
+        Parameters
+        ----------
+        order
+            The order of the quadrature, i.e. the highest degree polynomial
+            that is exactly integrated.
+
+        See also
+        --------
+        petsc.PetscQuadratureSetOrder
+
+        """
         cdef PetscInt corder = asInt(order)
         CHKERR( PetscQuadratureSetOrder(self.quad, corder))
 
--- src/binding/petsc4py/src/petsc4py/PETSc/FE.pyx
+++ src/binding/petsc4py/src/petsc4py/PETSc/FE.pyx
@@ -15,23 +15,88 @@ cdef class FE(Object):
         self.obj = <PetscObject*> &self.fe
         self.fe = NULL
 
-    def view(self, Viewer viewer=None):
+    def view(self, Viewer viewer=None) -> None:
+        """View a `FE` object
+
+        Collective.
+
+        Parameters
+        ----------
+        viewer
+            A `Viewer` to display the graph.
+
+        See also
+        --------
+        petsc.PetscFEView
+
+        """
         cdef PetscViewer vwr = NULL
         if viewer is not None: vwr = viewer.vwr
         CHKERR( PetscFEView(self.fe, vwr) )
 
-    def destroy(self):
+    def destroy(self) -> Self:
+        """Destroys the `FE` object
+
+        Collective.
+
+        See also
+        --------
+        petsc.PetscFEDestroy
+
+        """
         CHKERR( PetscFEDestroy(&self.fe) )
         return self
 
-    def create(self, comm=None):
+    def create(self, comm: Comm | None = None) -> Self:
+        """Creates an empty `FE` object.
+
+        The type can then be set with `setType`.
+
+        Collective.
+
+        Parameters
+        ----------
+        comm
+            The communicator for the `FE` object
+
+
+        See also
+        --------
+        petsc.PetscFECreate, setType
+
+        """
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
         cdef PetscFE newfe = NULL
         CHKERR( PetscFECreate(ccomm, &newfe) )
         PetscCLEAR(self.obj); self.fe = newfe
         return self
 
-    def createDefault(self, dim, nc, isSimplex, qorder, prefix=None, comm=None):
+    def createDefault(self, dim: int, nc: int, isSimplex: bool, qorder: int, prefix: str = None, comm: Comm | None = None) -> Self:
+        """Create a `FE` for basic FEM computation.
+
+        Collective.
+
+        Parameters
+        ----------
+        dim
+            The spatial dimension.
+        nc
+            The number of components.
+        isSimplex
+            Flag for simplex reference cell, otherwise its a tensor product.
+        qorder
+            The quadrature order or PETSC_DETERMINE (TODO: in params)to use `Space`
+            polynomial degree.
+        prefix
+            The options prefix, or None.
+        comm
+            The MPI communicator.
+
+        See also
+        --------
+        petsc.PetscFECreateDefault
+
+        """
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
         cdef PetscFE newfe = NULL
         cdef PetscInt cdim = asInt(dim)
@@ -45,7 +110,32 @@ cdef class FE(Object):
         PetscCLEAR(self.obj); self.fe = newfe
         return self
 
-    def createLagrange(self, dim, nc, isSimplex, k, qorder, comm=None):
+    def createLagrange(self, dim: int, nc: int, isSimplex: bool, k: int, qorder: int, comm=None) -> Self:
+        """Create a `FE` for the basic Lagrange space of degree k.
+
+        Collective.
+
+        Parameters
+        ----------
+        dim
+            The spatial dimension.
+        nc
+            The number of components.
+        isSimplex
+            Flag for simplex reference cell, otherwise its a tensor product.
+        k
+            The degree k of the space.
+        qorder
+            The quadrature order or `PETSC_DETERMINE` TODO: to use `Space`
+            polynomial degree.
+        comm
+            The MPI comm.
+
+        See also
+        --------
+        petsc.PetscFECreateLagrange
+
+        """
         cdef MPI_Comm ccomm = def_Comm(comm, PETSC_COMM_DEFAULT)
         cdef PetscFE newfe = NULL
         cdef PetscInt cdim = asInt(dim)
@@ -57,90 +147,331 @@ cdef class FE(Object):
         PetscCLEAR(self.obj); self.fe = newfe
         return self
 
-    def getQuadrature(self):
+    def getQuadrature(self) -> Quad:
+        """Return the `Quad` used to calculate inner products.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetQuadrature
+
+        """
         cdef Quad quad = Quad()
         CHKERR( PetscFEGetQuadrature(self.fe, &quad.quad) )
         return quad
 
-    def getDimension(self):
+    def getDimension(self) -> int:
+        """Return the dimension of the finite element space on a cell.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetDimension
+
+        """
         cdef PetscInt cdim = 0
         CHKERR( PetscFEGetDimension(self.fe, &cdim) )
         return toInt(cdim)
 
-    def getSpatialDimension(self):
+    def getSpatialDimension(self) -> int:
+        """Return the spatial dimension of the element.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetSpatialDimension
+
+        """
         cdef PetscInt csdim = 0
         CHKERR( PetscFEGetSpatialDimension(self.fe, &csdim) )
         return toInt(csdim)
 
-    def getNumComponents(self):
+    def getNumComponents(self) -> int:
+        """Return the number of components in the element.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetNumComponents
+
+        """
         cdef PetscInt comp = 0
         CHKERR( PetscFEGetNumComponents(self.fe, &comp) )
         return toInt(comp)
 
-    def setNumComponents(self, comp):
+    def setNumComponents(self, comp: int) -> None:
+        """Sets the number of field components in the element.
+
+        Not collective.
+
+        Parameters
+        ----------
+        comp
+            The number of field components.
+
+        See also
+        --------
+        petsc.PetscFESetNumComponents
+
+        """
         cdef PetscInt ccomp = asInt(comp)
         CHKERR( PetscFESetNumComponents(self.fe, comp) )
 
-    def getNumDof(self):
+    def getNumDof(self) -> ndarray:
+        """Return the number of dofs.
+
+        Return the number of dofs (dual basis vectors) associated to mesh
+        points on the reference cell of a given dimension.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetDimension, petsc.PetscFEGetNumDof
+
+        """
         cdef const PetscInt *numDof = NULL
         cdef PetscInt cdim = 0
         CHKERR( PetscFEGetDimension(self.fe, &cdim) )
         CHKERR( PetscFEGetNumDof(self.fe, &numDof) )
         return array_i(cdim, numDof)
 
-    def getTileSizes(self):
+    def getTileSizes(self) -> tuple(int, int, int, int):
+        """Return the tile sizes for evaluation.
+
+        Not collective.
+
+        Returns
+        -------
+        blockSize : int
+            The number of elements in a block.
+        numBlocks : int
+            The number of blocks in a batch.
+        batchSize : int
+            The number of elements in a batch.
+        numBatches : int
+            The number of batches in a chunk.
+
+        See also
+        --------
+        petsc.PetscFEGetTileSizes
+
+        """
         cdef PetscInt blockSize = 0, numBlocks = 0
         cdef PetscInt batchSize = 0, numBatches = 0
         CHKERR( PetscFEGetTileSizes(self.fe, &blockSize, &numBlocks, &batchSize, &numBatches) )
         return toInt(blockSize), toInt(numBlocks), toInt(batchSize), toInt(numBatches)
-    
-    def setTileSizes(self, blockSize, numBlocks, batchSize, numBatches):
+
+    def setTileSizes(self, blockSize: int, numBlocks: int, batchSize: int, numBatches: int) -> None:
+        """Sets the tile sizes for evaluation.
+
+        Not collective.
+
+        Parameters
+        ----------
+        blockSize
+            The number of elements in a block.
+        numBlocks
+            The number of blocks in a batch.
+        batchSize
+            The number of elements in a batch.
+        numBatches
+            The number of batches in a chunk.
+
+        See also
+        --------
+        petsc.PetscFESetTileSizes
+
+        """
         cdef PetscInt cblockSize = asInt(blockSize), cnumBlocks = asInt(numBlocks)
         cdef PetscInt cbatchSize = asInt(batchSize), cnumBatches = asInt(numBatches)
         CHKERR( PetscFESetTileSizes(self.fe, blockSize, numBlocks, batchSize, numBatches) )
 
-    def getFaceQuadrature(self):
+    def getFaceQuadrature(self) -> Quad:
+        """Return the `Quad` used to calculate inner products on faces.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetFaceQuadrature
+
+        """
         cdef Quad quad = Quad()
         CHKERR( PetscFEGetFaceQuadrature(self.fe, &quad.quad) )
         return quad
 
-    def setQuadrature(self, Quad quad):
+    def setQuadrature(self, Quad quad) -> Self:
+        """Sets the `Quad` used to calculate inner products.
+
+        Not collective.
+
+        Parameters
+        ----------
+        quad
+            The `Quad` object.
+
+        See also
+        --------
+        petsc.PetscFESetQuadrature
+
+        """
         CHKERR( PetscFESetQuadrature(self.fe, quad.quad) )
         return self
 
-    def setFaceQuadrature(self, Quad quad):
+    def setFaceQuadrature(self, Quad quad) -> Quad:
+        """Set the `Quad` used to calculate inner products on faces.
+
+        Not collective.
+
+        Parameters
+        ----------
+        quad
+            The `Quad` object
+
+        See also
+        --------
+        petsc.PetscFESetFaceQuadrature
+
+        """
         CHKERR( PetscFESetFaceQuadrature(self.fe, quad.quad) )
         return self
 
-    def setType(self, fe_type):
+    def setType(self, fe_type) -> Self:
+        """Build a particular `FE`.
+
+        Collective.
+
+        Parameters
+        ----------
+        fe_type
+            The kind of FEM space. TODO: params
+
+        See also
+        --------
+        petsc.PetscFESetType
+
+        """
         cdef PetscFEType cval = NULL
         fe_type = str2bytes(fe_type, &cval)
         CHKERR( PetscFESetType(self.fe, cval) )
         return self
 
-    def getBasisSpace(self):
+    def getBasisSpace(self) -> Space:
+        """Return the `Space` used for the approximation of the solution for the `FE`.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetBasisSpace
+
+        """
         cdef Space sp = Space()
         CHKERR( PetscFEGetBasisSpace(self.fe, &sp.space ) )
         return sp
-    
-    def setBasisSpace(self, Space sp):
+
+    def setBasisSpace(self, Space sp) -> None:
+        """Sets the `Space` used for the approximation of the solution.
+
+        Not collective.
+
+        Parameters
+        ----------
+        sp
+            The `Space` object.
+
+        See also
+        --------
+        petsc.PetscFESetBasisSpace
+
+        """
         CHKERR( PetscFESetBasisSpace(self.fe, sp.space ) )
 
-    def setFromOptions(self):
+    def setFromOptions(self) -> None:
+        """TODO
+
+        Collective.
+
+        Parameters
+        ----------
+        unit
+            Data type.
+
+        See also
+        --------
+        petsc.PetscFESetFromOptions
+
+        """
         CHKERR( PetscFESetFromOptions(self.fe) )
 
-    def setUp(self):
+    def setUp(self) -> None:
+        """TODO
+
+        Collective.
+
+        Parameters
+        ----------
+        unit
+            Data type.
+
+        See also
+        --------
+        petsc.PetscFESetUp
+
+        """
         CHKERR( PetscFESetUp(self.fe) )
 
-    def getDualSpace(self):
+    def getDualSpace(self) -> DualSpace:
+        """Return the `DualSpace` used to define the inner product for the `FE`.
+
+        Not collective.
+
+        See also
+        --------
+        petsc.PetscFEGetDualSpace, DualSpace
+
+        """
         cdef DualSpace dspace = DualSpace()
         CHKERR( PetscFEGetDualSpace(self.fe, &dspace.dualspace) )
         return dspace
-    
-    def setDualSpace(self, DualSpace dspace):
+
+    def setDualSpace(self, DualSpace dspace) -> None:
+        """Sets the `DualSpace` used to define the inner product.
+
+        Not collective.
+
+        Parameters
+        ----------
+        dspace
+            The `DualSpace` object
+
+        See also
+        --------
+        petsc.PetscFESetDualSpace, DualSpace
+
+        """
         CHKERR( PetscFESetDualSpace(self.fe, dspace.dualspace) )
 
-    def viewFromOptions(self, name, Object obj=None):
+    def viewFromOptions(self, name: str, Object obj=None) -> None:
+        """TODO
+
+        Collective.
+
+        Parameters
+        ----------
+        unit
+            Data type.
+
+        See also
+        --------
+        petsc.PetscFEViewFromOptions
+
+        """
         cdef const char *cname = NULL
         _ = str2bytes(name, &cname)
         cdef PetscObject  cobj = NULL
