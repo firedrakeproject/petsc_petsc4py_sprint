d3710bb5153f3d1f07711d189bd027b2b1493cc0
David Ham <David.Ham@imperial.ac.uk>
2023-03-30 11:10:20 +0100

    finished first pass of tutorial

--- src/binding/petsc4py/demo/poisson2d/poisson2d.py
+++ src/binding/petsc4py/demo/poisson2d/poisson2d.py
@@ -11,11 +11,10 @@
 # This is a naïve, parallel implementation, using :math:`n` interior grid
 # points per dimension and a lexicographic ordering of the nodes.
 
-# This code is kept as simple as possible to show the minimal differences with
-# the sequential version. However, simplicity comes at a price. Here we use a
-# naive decomposition that does not lead to an optimal communication complexity
-# for the matrix-vector product. See dmda.c for an example of optimal
-# communication complexity and nodes ordering.
+# This code is kept as simple as possible. However, simplicity comes at a
+# price. Here we use a naive decomposition that does not lead to an optimal
+# communication complexity for the matrix-vector product. An optimal complexity
+# decomposition of a structured grid could be achieved using `PETSc.DMDA`.
 #
 # This demo is structured as a script to be executed using:
 #
@@ -33,40 +32,67 @@ import petsc4py
 
 petsc4py.init(sys.argv)
 
-# import PETSc module
+# The full PETSc4py API is to be found in the `petsc4py.PETSc` module.
 
 from petsc4py import PETSc
 
-# Access options database
+# PETSc is extensively programmable using the `PETSc.Options` database. For
+# more information see `working with PETSc Options <petsc_options>`.
+
 OptDB = PETSc.Options()
 
-# Grid size and spacing
-# default values are specified within getters
+# Grid size and spacing using a default value of ``5```. The user can specify a
+# different number of points in each direction by passing the ``-n`` option to
+# the script.
+
 n = OptDB.getInt('n', 5)
 h = 1.0 / (n + 1)
 
-# Create sparse matrix
-# You can omit the comm argument if your objects leave on PETSc.COMM_WORLD
-# but it is a dangerous choice to rely on default values for such
-# important arguments
+# Sparse matrices are represented by `PETSc.Mat` objects.
+#
+# You can omit the ``comm`` argument if your objects live on
+# `PETSc.COMM_WORLD` but it is a dangerous choice to rely on default values
+# for such important arguments.
+
 A = PETSc.Mat()
 A.create(comm=PETSc.COMM_WORLD)
 
-# Specify global matrix size with tuple
+# Specify global matrix shape with a tuple.
+
 A.setSizes((n * n, n * n))
-# the call above implicitly assumes using PETSC_DECIDE for local sizes
-# and it is equivalent to
+
+# the call above implicitly assumes that we leave the parallel decomposition of
+# the matrix rows to PETSc by using `PETSc.DECIDE` for local sizes.
+# It is equivalent to:
+#
+# .. code-block:: python
+#
 #     A.setSizes(((PETSc.DECIDE, n * n), (PETSc.DECIDE, n * n)))
 
-# Set type and customize the object with command line
+# Various `sparse matrix formats <petsc4py.PETSc.Mat.Type>` can be selected:
+
 A.setType(PETSc.Mat.Type.AIJ)
+
+# Finally we allow the user to set any options they want to on the matrix from
+# the command line:
+
 A.setFromOptions()
 
-# Set preallocation
-# this call already handles the AIJ derived types for us
+# Insertion into some matrix types is vastly more efficient if we preallocate
+# space rather than allow this to happen dynamically. Here we hint the number
+# of nonzeros to be expected on each row.
+
 A.setPreallocationNNZ(5)
 
-index_to_grid = lambda r: (r // n, r % n)
+# We can now write out our finite difference matrix assembly using conventional
+# Python syntax. `Mat.getOwnershipRange` is used to retrieve the range of rows
+# local to this processor.
+
+
+def index_to_grid(r):
+    """Convert a row number into a grid point."""
+    return (r // n, r % n)
+
 
 rstart, rend = A.getOwnershipRange()
 for row in range(rstart, rend):
@@ -85,21 +111,43 @@ for row in range(rstart, rend):
         column = row + 1
         A[row, column] = -1.0 / h**2
 
+# At this stage, any parallel synchronization required in the matrix assembly
+# process has not occurred. We achieve this by calling `Mat.assemblyBegin` and
+# then `Mat.assemblyEnd`.
+
 A.assemblyBegin()
 A.assemblyEnd()
 
+# We set up an additional option so that the user can print the matrix by
+# passing ``-view_mat`` to the script.
+
 A.viewFromOptions('-view_mat')
 
-# Create Krylov solver context
+# PETSc represents all linear solvers as preconditioned Krylov subspace methods
+# of type `PETSc.KSP`. Here we create a KSP object for a conjugate gradient
+# solver preconditioned with an algebraic multigrid method.
+
 ksp = PETSc.KSP()
 ksp.create(comm=A.getComm())
 ksp.setType(PETSc.KSP.Type.CG)
 ksp.getPC().setType(PETSc.PC.Type.GAMG)
+
+# We set the matrix in our linear solver and allow the user to program the
+# solver with options.
+
 ksp.setOperators(A)
 ksp.setFromOptions()
 
-# Solve linear system
+# Since the matrix knows its size and parallel distribution, we can retrieve
+# appropriately-scaled vectors using `Mat.createVecs`. PETSc vectors are
+# objects of type `PETSc.Vec`. Here we set the right hand side of our system to
+# a vector of ones, and then solve.
+
 x, b = A.createVecs()
 b.set(1.0)
 ksp.solve(b, x)
+
+# Finally, allow the user to print the solution by passing ``-view_sol`` to the
+# script.
+
 x.viewFromOptions('-view_sol')
